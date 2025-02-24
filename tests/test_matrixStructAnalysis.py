8611e0169e7653765f45e0df14114d0d3b78af07
Rebecca Shannon <rebshan@bu.edu>
2025-02-24 09:52:22 -0500

    update tests

--- tests/test_matrixStructAnalysis.py
+++ tests/test_matrixStructAnalysis.py
@@ -10,10 +10,10 @@ from matrixStructAnalysis import Elements
 def NC():
     nodes = np.array([[8e3, 0, 0],[13e3, 0, 0]])
     load = np.array([[0,0,0,0,0,0],[5/math.sqrt(2),-5/math.sqrt(2),0,0,0,0]])
-    nodeType = np.array([1,0])
+    #nodeType = np.array([1,0])
     BC = np.zeros((2,6))
     BC[1,:] = 1
-    return Nodes(nodes=nodes,load=load,nodeType=nodeType,BC=BC)
+    return Nodes(nodes=nodes,load=load,BC=BC)
 
 @pytest.fixture
 def EC():
@@ -57,59 +57,59 @@ def test_find_global_frame_stiffness(NC,EC):
          0.00000000e+00, -9.60000000e+03,  0.00000000e+00, 0.00000000e+00,  0.00000000e+00,  3.20000000e+07]])
     assert np.allclose(known, found)
 
-def test_partition_matrices(NC,EC):
+# def test_partition_matrices(NC,EC):
 
-    K_global = np.zeros((18,18))
-    K_global[0] =+ 1
-    K_global[1] =+ 2
-    K_global[2] =+ 3
-    K_global[3] =+ 4
-    K_global[4] =+ 5
-    K_global[5] =+ 6
-    K_global[6] =+ 7
-    K_global[7] =+ 8
-    K_global[8] =+ 9
-    K_global[9] =+ 10
-    K_global[10] =+ 11
-    K_global[11] =+ 12
-    K_global[12] =+ 13
-    K_global[13] =+ 14
-    K_global[14] =+ 15
-    K_global[15] =+ 16
-    K_global[16] =+ 17
-    K_global[17] =+ 18
+#     K_global = np.zeros((18,18))
+#     K_global[0] =+ 1
+#     K_global[1] =+ 2
+#     K_global[2] =+ 3
+#     K_global[3] =+ 4
+#     K_global[4] =+ 5
+#     K_global[5] =+ 6
+#     K_global[6] =+ 7
+#     K_global[7] =+ 8
+#     K_global[8] =+ 9
+#     K_global[9] =+ 10
+#     K_global[10] =+ 11
+#     K_global[11] =+ 12
+#     K_global[12] =+ 13
+#     K_global[13] =+ 14
+#     K_global[14] =+ 15
+#     K_global[15] =+ 16
+#     K_global[16] =+ 17
+#     K_global[17] =+ 18
 
-    known, knownF,knownDOF = MSA.partition_matrices(NC,EC,K_global)
+#     known, knownF,knownDOF = MSA.partition_matrices(NC,EC,K_global)
 
-    found = np.zeros((18,18))
-    found[0] =+ 7
-    found[1] =+ 8
-    found[2] =+ 9
-    found[3] =+ 10
-    found[4] =+ 11
-    found[5] =+ 12
-    found[6] =+ 16
-    found[7] =+ 17
-    found[8] =+ 18
-    found[9] =+ 1
-    found[10] =+ 2
-    found[11] =+ 3
-    found[12] =+ 4
-    found[13] =+ 5
-    found[14] =+ 6
-    found[15] =+ 13
-    found[16] =+ 14
-    found[17] =+ 15
+#     found = np.zeros((18,18))
+#     found[0] =+ 7
+#     found[1] =+ 8
+#     found[2] =+ 9
+#     found[3] =+ 10
+#     found[4] =+ 11
+#     found[5] =+ 12
+#     found[6] =+ 16
+#     found[7] =+ 17
+#     found[8] =+ 18
+#     found[9] =+ 1
+#     found[10] =+ 2
+#     found[11] =+ 3
+#     found[12] =+ 4
+#     found[13] =+ 5
+#     found[14] =+ 6
+#     found[15] =+ 13
+#     found[16] =+ 14
+#     found[17] =+ 15
 
-    foundF=np.zeros((18,1))
-    foundF[6] = 5/math.sqrt(2)
-    foundF[7] = -5/math.sqrt(2)
+#     foundF=np.zeros((18,1))
+#     foundF[6] = 5/math.sqrt(2)
+#     foundF[7] = -5/math.sqrt(2)
 
-    foundDOF = 6 
+#     foundDOF = 6 
 
-    assert np.all(knownF == foundF)
-    assert knownDOF == foundDOF
-    assert np.all(known == found)
+#     assert np.all(knownF == foundF)
+#     assert knownDOF == foundDOF
+#     assert np.all(known == found)
 
 def test_run_MSA_solver(NC,EC):
     found_d,found_f = MSA.run_MSA_solver(NC,EC)
