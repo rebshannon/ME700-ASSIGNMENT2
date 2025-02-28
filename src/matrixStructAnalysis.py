487ae332a62ea27e22acc2a9b542c8e0aba66d9b
Rebecca Shannon <rebshan@bu.edu>
2025-02-28 16:32:14 -0500

    added tests

--- src/matrixStructAnalysis.py
+++ src/matrixStructAnalysis.py
@@ -52,25 +52,8 @@ def find_global_frame_stiffness(Nodes,Elements):
     K_global=np.zeros((Nodes.numNodes*6,Nodes.numNodes*6))
     for elm_ind in range(Elements.numElements):
 
-        # find element end points
-        node0 = Elements.connections[elm_ind][0]
-        node1 = Elements.connections[elm_ind][1]
-
-        x1 = Nodes.nodes[node0][0]
-        y1 = Nodes.nodes[node0][1]
-        z1 = Nodes.nodes[node0][2]
-        x2 = Nodes.nodes[node1][0]
-        y2 = Nodes.nodes[node1][1]
-        z2 = Nodes.nodes[node1][2]
-
-        # element length
-        Elements.L[elm_ind] = math.sqrt( (x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2 )
-
-        # DOF indices (add one for indexing)
-        node1_dof_ind1 = 6 * node0
-        node1_dof_ind2 = 6 * (node0 + 1)
-        node2_dof_ind1 = 6 * node1
-        node2_dof_ind2 = 6 * (node1 + 1)
+        # find element endpoints and associated DOF
+        x1,y1,z1,x2,y2,z2,node1_dof,node2_dof = find_element_endPoints_dof(Nodes, Elements, elm_ind)
 
         # material properties
         matProps = np.array([Elements.E[elm_ind], Elements.nu[elm_ind], Elements.A[elm_ind], Elements.L[elm_ind], Elements.Iy[elm_ind], Elements.Iz[elm_ind], Elements.J[elm_ind]])
@@ -79,13 +62,8 @@ def find_global_frame_stiffness(Nodes,Elements):
         k_e = MSA_math.local_elastic_stiffness_matrix_3D_beam(*matProps)
 
         # transformation matrix
-        # check for local z axis
-
-        if Elements.local_z is None:
-            gamma = MSA_math.rotation_matrix_3D(x1,y1,z1,x2,y2,z2)
-        else:
-            gamma = MSA_math.rotation_matrix_3D(x1,y1,z1,x2,y2,z2,Elements.local_z[elm_ind])
-    
+        # checks for local z axis      
+        gamma = find_gamma(x1,y1,z1,x2,y2,z2,Elements.local_z, elm_ind)
         Gamma = MSA_math.transformation_matrix_3D(gamma)
     
         # transform each element stiffness matrix to global coordinates
@@ -93,15 +71,42 @@ def find_global_frame_stiffness(Nodes,Elements):
                 
         # assemble frame stiffness matrix
 
-        K_global[node1_dof_ind1:node1_dof_ind2,node1_dof_ind1:node1_dof_ind2] += k_eg[0:6,0:6]
-        K_global[node1_dof_ind1:node1_dof_ind2,node2_dof_ind1:node2_dof_ind2] += k_eg[0:6,6:12]
-        K_global[node2_dof_ind1:node2_dof_ind2,node1_dof_ind1:node1_dof_ind2] += k_eg[6:12,0:6]
-        K_global[node2_dof_ind1:node2_dof_ind2,node2_dof_ind1:node2_dof_ind2] += k_eg[6:12,6:12]
+        K_global[node1_dof[0]:node1_dof[1],node1_dof[0]:node1_dof[1]] += k_eg[0:6,0:6]
+        K_global[node1_dof[0]:node1_dof[1],node2_dof[0]:node2_dof[1]] += k_eg[0:6,6:12]
+        K_global[node2_dof[0]:node2_dof[1],node1_dof[0]:node1_dof[1]] += k_eg[6:12,0:6]
+        K_global[node2_dof[0]:node2_dof[1],node2_dof[0]:node2_dof[1]] += k_eg[6:12,6:12]
 
     return K_global
 
-def partition_matrices(Nodes,Elements,K_global):
+def find_element_endPoints_dof(Nodes, Elements, elm_ind):
 
+    # find element end points
+    node0 = Elements.connections[elm_ind][0]
+    node1 = Elements.connections[elm_ind][1]
+
+    x1 = Nodes.nodes[node0][0]
+    y1 = Nodes.nodes[node0][1]
+    z1 = Nodes.nodes[node0][2]
+    x2 = Nodes.nodes[node1][0]
+    y2 = Nodes.nodes[node1][1]
+    z2 = Nodes.nodes[node1][2]
+
+    # element length
+    Elements.L[elm_ind] = math.sqrt( (x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2 )
+
+    # DOF indices (add one for indexing)
+    node1_dof = np.array([6 * node0, 6 * (node0 + 1)])
+    node2_dof = np.array([6 * node1, 6 * (node1 + 1)])
+    
+    return x1,y1,z1,x2,y2,z2,node1_dof, node2_dof
+
+def find_gamma(x1,y1,z1,x2,y2,z2,local_z, elm_ind):
+    if local_z is None: gamma = MSA_math.rotation_matrix_3D(x1,y1,z1,x2,y2,z2)
+    else: gamma = MSA_math.rotation_matrix_3D(x1,y1,z1,x2,y2,z2,local_z[elm_ind])
+    return gamma
+
+def partition_matrices(Nodes,Elements,K_global):
+    
     # partition forces 
     # these correspond to unknown displacement
     # keep track of their index with forceInd
@@ -135,15 +140,5 @@ def partition_matrices(Nodes,Elements,K_global):
         for cols, j in enumerate(forceInd):
             partK_forces[rows,cols] = K_global[i,j]
         dofInd.append(i)
-
-    # # assemble partitioned k_global based on which DOF are known (0) or unknown (1)
-    # # # should have the same indexing as the forceInd
-    # # unknown_disp = np.where(allDOF != 0)[0]
-    # known_disp = np.where(allDOF == 0)[0]
-
-    # # #partition_forces = np.vstack((Nodes.forces[unknown_disp],Nodes.forces[known_disp]))
-    # # #partition_K_global = np.vstack((K_global[unknown_disp],K_global[known_disp]))
-    # # partK_DOF = np.vstack((K_global[unknown_disp]))
-    # partK_forces = np.vstack((K_global[known_disp]))
 
     return partK_DOF,partK_forces, partF, forceInd, dofInd
